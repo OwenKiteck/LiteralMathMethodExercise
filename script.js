2660f363d59829032bcfaa7b6c18c991027874ce
Student <owen.kiteck@stu.barren.kyschools.us>
2022-02-15 16:13:35 -0600

    Finished exercise. Not bonus yet

---
+++ script.js
@@ -0,0 +1,18 @@
+let warmHugs = "Hi I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+
+console.log(warmHugs.replace("like", "love"));
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18, 36));
+
+let dotDotDot = "...";
+let skullBones = `I don't have a skull${dotDotDot} or bones.`
+console.log(skullBones);
+console.log(Math.PI);
+
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber += 1;
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
